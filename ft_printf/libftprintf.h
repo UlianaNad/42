35c1173913b9047acb845d538e5b037264bec687
Uliana Nadorozhna <unadoroz@c2r4p4.hive.fi>
2025-05-26 13:58:13 +0300

    added new folder - ft_printf

---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unadoroz <unadoroz@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 10:15:07 by unadoroz          #+#    #+#             */
+/*   Updated: 2025/05/26 10:16:42 by unadoroz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+
+int ft_printf(const char *, ...);
+
+#endif
